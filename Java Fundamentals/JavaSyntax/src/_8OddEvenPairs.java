779edf86e02efd2aca8ab935340a9aaf462ad126
ssgivanova <ssgivanova@gmail.com>
2015-10-22 22:51:37 +0300

    problem 8 & 9 added

--- Java Fundamentals/JavaSyntax/src/_8OddEvenPairs.java
+++ Java Fundamentals/JavaSyntax/src/_8OddEvenPairs.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class _8OddEvenPairs {
 
@@ -7,6 +9,28 @@ public class _8OddEvenPairs {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter some numbers :");
+		
+		String[] numbers = sc.nextLine().split(" ");
+		
+		if(numbers.length % 2 != 0){
+			System.out.println("Invalid length");
+		}else{
+			for (int i = 0; i < numbers.length - 1; i+=2) {
+				int n1 = Integer.parseInt(numbers[i]);
+				int n2 = Integer.parseInt(numbers[i+1]);
+				if(n1 % 2 == 0 && n2 % 2 == 0){
+					System.out.printf("%d, %d -> both are even\n",n1,n2);
+				}
+				else if(n1 % 2 != 0 && n2 % 2 != 0){
+					System.out.printf("%d, %d -> both are odd\n",n1,n2);
+				}
+				else {
+					System.out.printf("%d, %d -> different\n",n1,n2);
+				}
+			}
+		}
 	}
 
 }
--- Java Fundamentals/JavaSyntax/src/_9HitTheTarget.java
+++ Java Fundamentals/JavaSyntax/src/_9HitTheTarget.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class _9HitTheTarget {
 
@@ -7,7 +9,20 @@ public class _9HitTheTarget {
 	
 	public static void main(String[] args) {
 		
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the target ");
+		int target = sc.nextInt();
+		int max =20;
+		
+		for (int i = 1; i <= max; i++) {
+			for (int j = 0; j < max; j++) {
+				if(i + j == target){
+					System.out.printf("%d + %d = %d%n",i,j,target);
+				}
+				if(i - j == target  ){
+					System.out.printf("%d - %d = %d%n",i,j,target);
+				}
+			}
+		}
 	}
-
 }
